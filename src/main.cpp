39c910e55d8873a162320e3c9356334ec8c2e5fd
bznerd <guyinthechair631@gmail.com>
2022-12-27 17:55:44 -0500

    I did a stupid and didn't commit anything for ages... Sue me

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,121 @@
+#include <stdlib.h>
 #include <stdio.h>
-#include "pico/stdlib.h"
-#include "hardware/adc.h"
+#include <string>
 
+#include "tusb.h"
+
+#include "pico/stdlib.h"
+#include "hardware.h"
+
+
+//--------------------------------------------------------------------+
+// MACRO CONSTANT TYPEDEF PROTYPES
+//--------------------------------------------------------------------+
+
+/* Blink pattern
+ * - 250 ms  : device not mounted
+ * - 1000 ms : device mounted
+ * - 2500 ms : device is suspended
+ */
+enum {
+  BLINK_NOT_MOUNTED = 250,
+  BLINK_MOUNTED = 1000,
+  BLINK_SUSPENDED = 2500
+};
+
+
+static uint32_t blink_interval_ms = BLINK_NOT_MOUNTED;
+
+void hid_task();
 
 //Program entry point
 int main() {
-  //Initialize USB serial coms and wait for connection
-  stdio_init_all();
-  while( !stdio_usb_connected() ) sleep_ms(10);
-
-  //Initialize and configure analog (ADC) input and GPIOs
-  adc_init();
-  adc_gpio_init(26);
-  adc_select_input(0);
+  hardware_init();
+  tusb_init();
   
-  for(int i = 0; i < 4; i++){
-    gpio_set_dir(i, false);
-    gpio_pull_up(i);
-  }
-
   //Main loop
   while(true){
-    //Poll the buttons
-    for(int i = 0; i < 4; i++){
-      if(!gpio_get(i)) printf("GPIO %d\n",i);
-    }
-    //Poll the ADC
-    printf("Pot value: %d\n", adc_read());
-    sleep_ms(250);
+    hid_task();
+    tud_task();
+    led_blinking_task(blink_interval_ms);
   }
   return 0;
+}
+
+//--------------------------------------------------------------------+
+// Device callbacks
+//--------------------------------------------------------------------+
+
+// Invoked when device is mounted
+void tud_mount_cb(void) {
+  blink_interval_ms = BLINK_MOUNTED;
+}
+
+// Invoked when device is unmounted
+void tud_umount_cb(void) {
+  blink_interval_ms = BLINK_NOT_MOUNTED;
+}
+
+// Invoked when usb bus is suspended
+// remote_wakeup_en : if host allow us  to perform remote wakeup
+// Within 7ms, device must draw an average of current less than 2.5 mA from bus
+void tud_suspend_cb(bool remote_wakeup_en) {
+  (void) remote_wakeup_en;
+  blink_interval_ms = BLINK_SUSPENDED;
+}
+
+// Invoked when usb bus is resumed
+void tud_resume_cb(void) {
+  blink_interval_ms = BLINK_MOUNTED;
+}
+
+//--------------------------------------------------------------------+
+// USB HID
+//--------------------------------------------------------------------+
+
+// Every 10ms send a HID Report
+void hid_task(void) {
+    // Poll every 10ms
+    const uint32_t interval_ms = 10;
+    static uint32_t start_ms = 0;
+
+    if (( to_ms_since_boot(get_absolute_time()) - start_ms) < interval_ms) return; // not enough time
+    start_ms = to_ms_since_boot(get_absolute_time()) + interval_ms;
+
+    // Remote wakeup
+    if (tud_suspended()) {
+        // Wake up host if we are in suspend mode
+        // and REMOTE_WAKEUP feature is enabled by host
+        tud_remote_wakeup();
+    }
+
+    /*------------- Joystick -------------*/
+    if (tud_hid_ready()) {
+      tud_hid_gamepad_report(0x00, get_twist(), 0, 0, 0, 0, 0, 0, get_buttons());
+    }
+}
+
+// Invoked when received GET_REPORT control request
+// Application must fill buffer report's content and return its length.
+// Return zero will cause the stack to STALL request
+uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t reqlen) {
+  // TODO not Implemented
+  (void) instance;
+  (void) report_id;
+  (void) report_type;
+  (void) buffer;
+  (void) reqlen;
+
+  return 0;
+}
+
+// Invoked when received SET_REPORT control request or
+// received data on OUT endpoint ( Report ID = 0, Type = 0 )
+void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize) {
+  (void) instance;
+  (void) report_id;
+  (void) report_type;
+  (void) buffer; 
+  (void) bufsize;
+
 }
